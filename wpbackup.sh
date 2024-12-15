907da070f4dbad4d0ed71e441480c13842f7112a
Billiux <59266814+Billiux@users.noreply.github.com>
2024-12-14 21:45:17 -0500

    Update wpbackup.sh

--- wpbackup.sh
+++ wpbackup.sh
@@ -67,7 +67,7 @@ if [[ $? -eq 0 ]]; then
     
     Back up sucessfully created.
     Back up file name: $ZIP_FILE
-    The website backup is located in the parent directory of public_html file."
+    The website backup is located in the parent directory of the public_html file."
 else
     echo "Error: Failed to zip public_html folder."
     exit 1
