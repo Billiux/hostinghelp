e7c0e56d55326e9c9389d26591058886b6f354a9
Billiux <59266814+Billiux@users.noreply.github.com>
2024-12-16 20:50:57 -0500

    Update dbdump.sh

--- dbdump.sh
+++ dbdump.sh
@@ -25,22 +25,22 @@ if [[ -z "$DB_NAME" || -z "$DB_USER" || -z "$DB_PASSWORD" || -z "$DB_HOST" ]]; t
 fi
 
 # Define backup file name with timestamp
-BACKUP_FILE="db-backup-$(date +%Y%m%d%H%M%S).sql"
+BACKUP_FILE="dbdump-$(date +%Y%m%d%H%M%S).sql"
 
 # Perform the database dump
 mysqldump -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASSWORD" "$DB_NAME" > "$BACKUP_FILE"
 
 # Check if mysqldump was successful
 if [[ $? -eq 0 ]]; then
-    echo "Database backup successful: $BACKUP_FILE"
+    echo "    Database backup successful: $BACKUP_FILE"
 else
     echo "Error: Database backup failed."
     exit 1
 fi
 
 # Navigate one level up
-cd ..
+#cd ..
 
 # Print the current directory
 CURRENT_DIR=$(pwd)
-echo "Navigated to parent directory: $CURRENT_DIR"
+echo "    DB dump file called: $BACKUP_FILE"
